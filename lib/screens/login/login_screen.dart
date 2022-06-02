0834be89f69e9c0d4e276409947b70d60a2a49cc
abdell <hilmiabdelfattah@gmail.com>
2022-06-03 00:37:36 +0100

    login page in progress

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            width: width,
+            height: height*0.27,
+            decoration: const BoxDecoration(
+              color: Colors.amberAccent,
+            ),
+          ),
+          Container(
+            width: width,
+            margin: const EdgeInsets.only(left: 20,right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children:  [
+                const Text(
+                  "Hello",
+                  style: TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  ),
+                  Text(
+                  "Sign-in to your account",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey[500],
+                  ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Container(
+                    decoration:  BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 10,
+                          offset: const Offset(1, 1),
+                          color: Colors.grey.withOpacity(0.5),
+                        )
+                        ]
+                    ),
+                    child: TextField(
+                      cursorColor: Colors.grey,
+                      style: const TextStyle(
+                        fontSize: 20,
+                      ),
+                      textAlignVertical: TextAlignVertical.center,
+                      decoration: InputDecoration(
+                        focusedBorder:  OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: const BorderSide(
+                            color: Colors.white
+                          )
+                        ),
+                        enabledBorder:  OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide: const BorderSide(
+                            color: Colors.white
+                          )
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30)
+                        )
+                      ),
+                    ),
+                  )
+              ],
+            ),  
+          )
+        ],
+      )
+    );
+  }
+}
